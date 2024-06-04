13fd6b9f4fcad576329503bc39226e78d111db33
lucasFreitasDev97 <lukasm2@live.com>
2024-06-04 19:22:28 -0300

    create routes to suppliers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SupplierController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('suppliers/index', [SupplierController::class, 'index'])->name('suppliers.index');
+Route::get('suppliers/show/{supplier}', [SupplierController::class, 'show'])->name('suppliers.show');
+Route::get('suppliers/create', [SupplierController::class, 'create'])->name('suppliers.create');
+Route::post('suppliers/store/{supplier}', [SupplierController::class, 'store'])->name('suppliers.store');
+Route::get('suppliers/edit/{supplier}', [SupplierController::class, 'edit'])->name('suppliers.edit');
+Route::put('suppliers/update/{supplier}', [SupplierController::class, 'update'])->name('suppliers.update');
+Route::delete('suppliers/destroy/{destroy}', [SupplierController::class, 'destroy'])->name('suppliers.destroy');
